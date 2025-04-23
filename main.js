c2eee5cec6ba50cb435f27b0ae1c514af31c9720
Anil Tyata <tyata.anil@gmail.com>
2025-04-22 20:06:51 -0400

    Initial commit

--- main.js
+++
@@ -1,9 +0,0 @@
-// Select the hamburger icon and the navigation links container
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('navLinks');
-
-// Add a click event listener to the hamburger icon
-hamburger.addEventListener('click', () => {
-  // Toggle the 'active' class on the navigation links container
-  navLinks.classList.toggle('active');
-});
